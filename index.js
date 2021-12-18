c2fcfbc5f2c7f21cb3095bb08036d4f41861861f
Hossein Akbari <h.akbari@khodro45.com>
2021-12-18 13:16:15 +0330

    handle LTR force

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
  * @format
  */
 
-import {AppRegistry, LogBox} from 'react-native';
+import {AppRegistry, LogBox, I18nManager} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+I18nManager.allowRTL(false);
+I18nManager.forceRTL(false);
 
 LogBox.ignoreLogs(['Remote debugger'])
 LogBox.ignoreAllLogs(true)
